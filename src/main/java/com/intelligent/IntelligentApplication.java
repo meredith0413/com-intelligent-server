d9307425861240a364b86bfb8e7351cce3afc7b3
MiaoK <1131995345@qq.com>
2025-04-11 15:47:47 +0800

    提交

--- src/main/java/com/intelligent/IntelligentApplication.java
+++ src/main/java/com/intelligent/IntelligentApplication.java
@@ -1,9 +1,11 @@
 package com.intelligent;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.intelligent.mapper")
 public class IntelligentApplication {
 
 	public static void main(String[] args) {
